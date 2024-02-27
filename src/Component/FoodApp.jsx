2ab436586a5bf3aa8a1826c7e8721c1dfa57b2c1
Vasanti <vasanti.bendre06@gmail.com>
2024-02-27 17:57:08 +0530

    commit

--- src/Component/FoodApp.jsx
+++ src/Component/FoodApp.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React from "react";
 import { db } from "../utils/firebase";
 import { ref, onValue, child } from "firebase/database";
 
 const FoodApp = () => {
-    const handleDB = async () => {
-            const dbRef = ref(db, "recipies");
-            onValue(dbRef, (snapshot) => {
-                const data = snapshot.val();
-                console.log(data);
-            });
-    }
+  const handleDB = async () => {
+    const dbRef = ref(db, "recipies");
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+  };
   return (
     <>
-    <div className='flex items-center justify-center h-[30vh]'>
-        <div className=''>
-            <input className=' bg-gray-200 w-[40rem] outline-none px-2 rounded-md py-2 ' type="text" placeholder='search your recipeis' />
-            <button onClick={handleDB} className='border px-2 py-2 bg-black text-white hover:shadow-xl cursor-pointer transition-all delay-100 duration-100'>Search</button>
+      <div className="flex items-center justify-center h-[30vh]">
+        <div className="">
+          <input
+            className=" bg-gray-200 w-[40rem] outline-none px-2 rounded-md py-2 "
+            type="text"
+            placeholder="search your recipeis"
+          />
+          <button
+            onClick={handleDB}
+            className="border px-2 py-2 bg-black text-white hover:shadow-xl cursor-pointer transition-all delay-100 duration-100"
+          >
+            Search
+          </button>
         </div>
-    </div>
+      </div>
+      <div className="flex justify-center px-5 py-5">
+        <div className="left bg-orange-300 w-[350px] h-[50vh]">
+            <div className="flex items-center space-x-3">
+                <img className="w-[40px] h-[40px] rounded-full  " src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg?w=400&h=300&c=crop" alt="" />
+                <div>
+                    <h1>Hello image</h1>
+                    <h2>by Vasanti Beadre</h2>
+                </div>
+            </div>
+        </div>
+        <div className="right bg-black w-[350px] h-[50vh]  "></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default FoodApp
+export default FoodApp;
--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -1,64 +1,98 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Profile = () => {
-    const [personType, setpersonType] = useState("");
+  const [personType, setpersonType] = useState("");
   return (
-    <div className='flex items-center justify-center h-[100vh] w-[60%] mx-auto '>
+    <div className="flex items-center justify-center h-[100vh] w-[60%] mx-auto ">
       <form onSubmit={(e) => e.preventDefault()} action="">
-        <h1 className='text-center'>To Complete Your Profile </h1>
-        <div className='mt-4'>
-            <label  htmlFor="">Full Name</label>
-            <input className='block mt-2 outline-none px-2 rounded-md bg-gray-300 w-full py-1 ' type="text" />
+        <h1 className="text-center">To Complete Your Profile </h1>
+        <div className="mt-4">
+          <label htmlFor="">Full Name</label>
+          <input
+            className="block mt-2 outline-none px-2 rounded-md bg-gray-300 w-full py-1 "
+            type="text"
+          />
         </div>
-        <div className='mt-2'>
-            <label htmlFor="">Age</label>
-            <input className='block outline-none px-2 rounded-md bg-gray-300 w-full py-1 mt-2' type="text" />
+        <div className="mt-2">
+          <label htmlFor="">Age</label>
+          <input
+            className="block outline-none px-2 rounded-md bg-gray-300 w-full py-1 mt-2"
+            type="text"
+          />
         </div>
-        <div className='mt-2'>
-            <label htmlFor="">Weight</label>
-            <input className='block bg-gray-300 outline-none  w-full rounded-md py-1 mt-2' type="text" />
+        <div className="mt-2">
+          <label htmlFor="">Weight</label>
+          <input
+            className="block bg-gray-300 outline-none  w-full rounded-md py-1 mt-2"
+            type="text"
+          />
         </div>
-        <div className='mt-2'>
-            <label htmlFor="">height</label>
-            <input className='block bg-gray-300 outline-none  w-full rounded-md py-1 mt-2' type="text" />
+        <div className="mt-2">
+          <label htmlFor="">height</label>
+          <input
+            className="block bg-gray-300 outline-none  w-full rounded-md py-1 mt-2"
+            type="text"
+          />
         </div>
-        <div className='mt-2'>
-            <label>Gender : </label>
-            <select className='w-full rounded-md py-2 px-2 mt-2' name="" id="">
-                <option value="">Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-            </select>
+        <div className="mt-2">
+          <label>Gender : </label>
+          <select className="w-full rounded-md py-2 px-2 mt-2" name="" id="">
+            <option value="">Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
         </div>
-        <div className='mt-2'>
-            <label htmlFor="">Person Type</label>
-            <select onChange={(e) => setpersonType(e.currentTarget.value)} className='w-full py-2 px-2 mt-2'  name="" id="">
-                <option value="">Chooose the value</option>
-                <option value="Family">Family</option>
-                <option value="Individual">Individual</option>
-            </select>
+        <div className="mt-2">
+          <label htmlFor="">Person Type</label>
+          <select
+            onChange={(e) => setpersonType(e.currentTarget.value)}
+            className="w-full py-2 px-2 mt-2"
+            name=""
+            id=""
+          >
+            <option value="">Chooose the value</option>
+            <option value="Family">Family</option>
+            <option value="Individual">Individual</option>
+          </select>
         </div>
-        <div className='mt-4'>
-            <label className='font-bold' htmlFor="">Do you have any Health Issues</label>
-            <div>
-               <input type='checkbox' /> Thyroid 
-            </div>
-            <div>
+        <div className="mt-4">
+          <label className="font-bold" htmlFor="">
+            Do you have any Health Issues
+          </label>
+          <div>
+            <input type="checkbox" /> Thyroid
+          </div>
+          <div>
             <input type="checkbox" /> Blood Pressure
-            </div>
-            <div>
+          </div>
+          <div>
             <input type="checkbox" /> Diabetes
-            </div>
+          </div>
         </div>
-       {personType === "Family" && <div className='
-       mt-4'>
-        <label htmlFor="">How Many Members</label>
-        <input className='block w-full bg-gray-300 py-2 px-2 mt-2 outline-none' type="number" placeholder='enter your family member' />
-        </div>}
-        {personType === "Family" && <button className='border bg-purple-400 text-white py-2 mt-2 rounded-md px-2 w-full' >Add person details</button> }
+        {personType === "Family" && (
+          <div
+            className="
+       mt-4"
+          >
+            <label htmlFor="">How Many Members</label>
+            <input
+              className="block w-full bg-gray-300 py-2 px-2 mt-2 outline-none"
+              type="number"
+              placeholder="enter your family member"
+            />
+          </div>
+        )}
+        {personType === "Family" && (
+          <button className="border bg-purple-400 text-white py-2 mt-2 rounded-md px-2 w-full">
+            Add person details
+          </button>
+        )}
+        {personType === "Individual" && (
+          <button className="border bg-purple-400 text-white py-2 mt-2 rounded-md px-2 w-full">Submit your details</button>
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
